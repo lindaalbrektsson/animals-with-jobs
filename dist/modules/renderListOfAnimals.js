4446c77cc3881a2132dc7ec0ef4dba14634395bb
Linda Albrektsson <linda.albrektsson@gmail.com>
2026-04-07 11:53:27 +0200

    Basics and iAnimal.js

---
+++ dist/modules/renderListOfAnimals.js
@@ -0,0 +1,5 @@
+//Titta i index.html och hitta den UL där listan på djur ska skrivas ut.
+//Rendera ut en lista över djurens namn på skärmen
+//Varje list-item ska ha en event-lyssnare som kallar på "renderAnimalInfo"
+//Typa upp funktionens parameter
+export default function renderListOfAnimals(animals) { }
