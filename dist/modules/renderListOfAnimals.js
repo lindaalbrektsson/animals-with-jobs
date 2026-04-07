319f5ac31235a40d1364ed26f925ba1a72fc726b
Linda Albrektsson <linda.albrektsson@gmail.com>
2026-04-07 12:34:43 +0200

    renderListOfAnimals

--- dist/modules/renderListOfAnimals.js
+++ dist/modules/renderListOfAnimals.js
@@ -2,4 +2,17 @@
 //Rendera ut en lista över djurens namn på skärmen
 //Varje list-item ska ha en event-lyssnare som kallar på "renderAnimalInfo"
 //Typa upp funktionens parameter
-export default function renderListOfAnimals(animals) { }
+import renderAnimalInfo from "./renderAnimalInfo";
+export default function renderListOfAnimals(animals) {
+    const animalListContainer = document.querySelector(".list-of-animals ul");
+    if (!animalListContainer)
+        return;
+    for (const animal of animals) {
+        const li = document.createElement("li");
+        li.textContent = animal.name;
+        li.addEventListener("click", () => {
+            renderAnimalInfo(animal);
+        });
+        animalListContainer.appendChild(li);
+    }
+}
--- src/modules/renderListOfAnimals.ts
+++ src/modules/renderListOfAnimals.ts
@@ -3,4 +3,22 @@
 //Varje list-item ska ha en event-lyssnare som kallar på "renderAnimalInfo"
 //Typa upp funktionens parameter
 
-export default function renderListOfAnimals(animals) {}
+import { IAnimal } from "./IAnimal";
+import renderAnimalInfo from "./renderAnimalInfo";
+
+export default function renderListOfAnimals(animals: IAnimal[]): void {
+  const animalListContainer = document.querySelector(".list-of-animals ul");
+
+  if (!animalListContainer) return;
+
+  for (const animal of animals) {
+    const li = document.createElement("li");
+    li.textContent = animal.name;
+
+    li.addEventListener("click", () => {
+      renderAnimalInfo(animal);
+    });
+
+    animalListContainer.appendChild(li);
+  }
+}
